e1ca4a985849361452024d60109083f8d65c2187
ArminJaemaa <ajaemaa@tlu.ee>
2025-02-07 20:29:02 +0200

    controller, entity, repository tehtud veebipood

---
+++ 01_07.02_andmemudelid/veebipood/src/main/java/ee/jaemaa/veebipood/entity/Product.java
@@ -0,0 +1,45 @@
+package ee.jaemaa.veebipood.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+//hibernate
+//automaatselt tekib andmebaasi tabel, mis on klassi nimega
+//settings -> plugins -> JPA buddy -> install
+
+//Andmebaasi sisestatakse:
+//boolean
+
+//Sting
+//char
+
+//int -> 2.1miljardit
+//long ->
+//short -> 128
+//byte -> 32
+
+//float -> . 8 kohta
+//double -> . 16 kohta
+
+//private ei vaja Getterit ega Setterit
+@Getter
+@Setter //encapsulation
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double price;
+    private String image;
+    private boolean active;
+
+}
